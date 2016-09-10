7ad2480b06bb0b4fdf3996982b6bd86809d2a0b6
anthony.ras <ayolachll@gmail.com>
2016-09-10 11:27:29 +0800

    changes for about us applied.

--- bin/gt_register_menus.php
+++ bin/gt_register_menus.php
@@ -23,7 +23,7 @@ function clean_custom_menus($menu_name) {
 	if (($locations = get_nav_menu_locations()) && isset($locations[$menu_name])) {
 		$menu = wp_get_nav_menu_object($locations[$menu_name]);
 		$menu_items = (array) wp_get_nav_menu_items($menu->term_id);
-
+		
 		foreach ($menu_items as $key => $menu_item) {
 			$title = format_title($menu_item->title);
 			if($key == 3) {
